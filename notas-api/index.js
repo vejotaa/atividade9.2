c2ff7e4d73e0071623fc9954899c7e553b12c7b0
vejota <teslawolf@tutanota.com>
2025-06-03 00:31:00 -0300

    Modifica index.js, bugfixes

--- notas-api/index.js
+++ notas-api/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 const FILE_PATH = './notas.json';
 
